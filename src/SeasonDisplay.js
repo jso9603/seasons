2fe328daa480b9ced26d0c8d59969da92c7aef3a
JISOO PARK <jso9603@gmail.com>
2019-01-11 12:12:24 +0900

    Add icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    lat > 0 ? 'summer' : 'winter';
+    return lat > 0 ? 'summer' : 'winter';
   } else {
     return lat > 0 ? 'winter' : 'summer';
   }
@@ -11,10 +11,13 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+  const icon = season === 'winter' ? 'snowflake' : 'sun'
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
